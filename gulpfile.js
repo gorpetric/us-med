5ad13f016afb58824f03b5f5176a0ec9c99afc4a
Goran <gorpetric@foi.hr>
2015-11-05 16:06:32 +0100

    Gulp and some style stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
-var elixir = require('laravel-elixir');
+//var elixir = require('laravel-elixir');
+var gulp 		= require('gulp'),
+	browserSync = require('browser-sync'),
+	sass		= require('gulp-sass'),
+	prefix		= require('gulp-autoprefixer'),
+	minifyCss	= require('gulp-minify-css'),
+	rename		= require('gulp-rename')
+	cp			= require('child_process');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +18,37 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
+/*elixir(function(mix) {
     mix.sass('app.scss');
+});*/
+
+gulp.task('browser-sync', function(){
+	browserSync({
+		proxy: {
+			target: "localhost/us-med/public/"
+		}
+	});
 });
+
+gulp.task('sass', function(){
+	return gulp.src('resources/assets/css/app.scss')
+		.pipe(sass({
+			includePaths: ['css'],
+			onError: browserSync.notify
+		}))
+		.pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(gulp.dest('public/css'))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifyCss())
+		.pipe(gulp.dest('public/css'))
+		.pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('resources/assets/css/**', ['sass']);
+	gulp.watch('resources/views/**').on('change', browserSync.reload);
+	//gulp.watch('public/js/**').on('change', browserSync.reload);
+	//gulp.watch(['app/**', 'public/**']).on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Udruga studenata Međimurje</title>
-
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <link rel="shortcut icon" href="img/favicon.ico" />
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="css/app.min.css">
+        <title>Udruga studenata Međimurje</title>
         <style>
             html, body {
                 height: 100%;
