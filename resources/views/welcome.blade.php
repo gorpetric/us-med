5d93f1f79230b5522273a82d4da21849b1cb03dc
Goran <gorpetric@foi.hr>
2015-11-05 15:30:55 +0100

    Changed title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Udruga studenata Međimurje</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
