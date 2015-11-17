430f9202ff2d019dae1be757b6bacbee54c5a6e6
Goran <gorpetric@foi.hr>
2015-11-17 15:12:39 +0100

    PACE preloader animation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,6 +22,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/dropzone.js"></script>
 <script src="{{ asset('js/lightbox.min.js') }}"></script>
 <script src="{{ asset('js/sweetalert.min.js') }}"></script>
+<script src="{{ asset('js/pace.min.js') }}"></script>
 @include('partials.swal')
 @yield('js')
 </body>
