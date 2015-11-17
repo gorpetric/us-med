310b9146b4555397ae0bf282c2c57ae02592975a
Goran <gorpetric@foi.hr>
2015-11-17 13:30:22 +0100

    Added footer first design

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,14 +1,38 @@
 <footer>
 	<div class="container">
+		<a title='Find us on Facebook' target='_blank' href="https://www.facebook.com/UdrugaStudenataMedjimurja"><img class='fb-find' src="{{ asset('img/social-media-icons/fb-find.png') }}" /></a>
 		<div class="row">
-			<div class="col-sm-6">
-				<p>Partneri</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde delectus ducimus quis, ea, earum vitae cum voluptatum facilis laborum? Quia veniam dolore esse commodi odio facilis iusto vel, consectetur quidem.</p>
+			<div class='col-sm-5 col-lg-3 col-lg-offset-2 col-md-4 col-md-offset-1'>
+				<p>Studentske grupe</p>
+				<a target='_blank' href="https://www.facebook.com/groups/267580743281222">Udruga studenata Međimurja</a><br/>
+				<a target='_blank' href="https://www.facebook.com/groups/267580743281222">Studentski dom "Stjepan Radić"</a><br/>
+				<a target='_blank' href="https://www.facebook.com/groups/219249268169861/">Ekipa s Cvjetnog</a><br/>
+				<a target='_blank' href="https://www.facebook.com/groups/S.D.Shara/">Studentski dom Ante Starčević - Šara</a><br/>
+				<a target='_blank' href="https://www.facebook.com/groups/7584929309/">Studentski dom Lašćina</a><br/>
 			</div>
-			<div class="col-sm-6">
+			<div class='col-sm-4 col-lg-3 col-md-3'>
+				<p>Korisne grupe</p>
+				<a target='_blank' href="https://www.facebook.com/groups/trazim.nudim.prijevoz/">Tražim / nudim prijevoz</a><br/>
+				<a target='_blank' href="https://www.facebook.com/groups/141959189166899">Tražim cimera / cimericu</a>
+			</div>
+			<div class='col-sm-3 col-lg-2 col-md-2'>
 				<p>Korisni linkovi</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus atque sint earum temporibus adipisci corporis impedit harum praesentium consectetur tempore necessitatibus excepturi, optio eveniet officia illum, repellat, quasi nulla ipsa.</p>
+				<a target='_blank' href="http://www.zet.hr">ZET</a><br/>
+				<a target='_blank' href="https://pretinac.gov.hr/KorisnickiPretinac/eGradani.html">e-Građani</a><br/>
+				<a target='_blank' href="http://www.sczg.unizg.hr/">Studentski Centar</a><br/>
+				<a target='_blank' href="http://esava.info/">e-Sava</a><br/>
+				<a target='_blank' href="http://toastmasters.hr/">Toastmasters</a><br/>
 			</div>
+		</div>
+		<hr/>
+		<div class="partners">
+			<div class="partner"><a title="Općina Nedelišće" target='_blank' href="http://nedelisce.hr/"><img class='img-responsive' src="{{ asset('img/partneri/opcina_nedelisce.gif') }}" /></a></div>
+			<div class="partner"><a title="Međimurske vode" target='_blank' href="http://medjimurske-vode.hr/"><img class='img-responsive' src="{{ asset('img/partneri/med-vode.png') }}" /></a></div>
+			<div class="partner"><a title="Međimurje plin" target='_blank' href="http://www.medjimurje-plin.hr/"><img class='img-responsive' src="{{ asset('img/partneri/med-plin.png') }}" /></a></div>
+			<div class="partner"><a title="C-Lab" target='_blank' href="http://clab.hr/"><img class='img-responsive' src="{{ asset('img/partneri/clab.png') }}" /></a></div>
+			<div class="partner"><a title="Međimurska županija" target='_blank' href="http://medjimurska-zupanija.hr/"><img class='img-responsive' src="{{ asset('img/partneri/med-zupanija.gif') }}" /></a></div>
+			<div class="partner"><a title="Grad Čakovec" target='_blank' href="http://www.cakovec.hr/"><img class='img-responsive' src="{{ asset('img/partneri/grad-cakovec.gif') }}" /></a></div>
+			<div class="partner"><a title="Hortus Croatiae" target='_blank' href="http://projekti.hgk.hr/projects/9206-hortus-croatiae-vrt-hrvatske"><img class='img-responsive' src="http://placehold.it/100x100" /></a></div>
 		</div>
 	</div>
 </footer>
