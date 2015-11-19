2ebe491d5c9b1b0758014c09e33636556f732679
Goran <gorpetric@foi.hr>
2015-11-19 23:00:05 +0100

    News and each story first design

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -15,11 +15,35 @@
 	@if(!$news->count())
 		<p>Trenutno nema ni jedne vijesti.</p>
 	@else
-		@foreach ($news as $story)
-			<h4><a href="{{ route('news.story', ['slug' => $story->slug]) }}">{{ $story->title }}</a></h4>
-			{{ stripMarkdown(str_limit($story->body, 250)) }}
-			<p>{{ $story->created_at->diffForHumans() }}</p>
-		@endforeach
+		<div class="vijesti">
+			<div class="prva">
+				<div class="row">
+					<div class="col-sm-6">
+						<img class='img-responsive img-rounded' src="{{ asset('img/news/' . $news[0]->image) }}">
+					</div>
+					<div class="col-sm-6">
+						<h3>
+							<small>{{ $news[0]->created_at->format('d.m.Y. H:i') }}</small><br/>
+							<a href="{{ route('news.story', ['slug' => $news[0]->slug]) }}">{{ $news[0]->title }}</a>
+						</h3>
+						<p>{{ stripMarkdown(str_limit($news[0]->body, 300)) }}</p>
+						<a class='btn btn-default' href="{{ route('news.story', ['slug' => $news[0]->slug]) }}">Pročitaj vijest</a>
+					</div>
+				</div>
+			</div>
+			<div class="ostale">
+				@if($news->count() >= 1)
+				<hr style='border-color:#262626'>
+				@for ($i=1; $i < $news->count(); $i++)
+					<a class='ostala' href="{{ route('news.story', ['slug' => $news[$i]->slug]) }}">
+						<h4>{{ $news[$i]->title }}</h4>
+						<img class='img-responsive img-rounded' src="{{ asset('img/news/' . $news[$i]->image) }}">
+						<p>{{ $news[$i]->created_at->format('d.m.Y. H:i') }}</p>
+					</a>
+				@endfor
+				@endif
+			</div>
+		</div>
 	@endif
 </div>
 @stop
--- resources/views/news/story.blade.php
+++ resources/views/news/story.blade.php
@@ -5,8 +5,24 @@
 @section('content')
 <div class="vijest">
 <div class="container">
-	<img class="img-responsive img-rounded vijest-banner" src="{{ asset('img/news/' . $story->image) }}">
-	<h2 style='text-align:center'><small>Vijest</small><br/>{{ $story->title }}</h2>
+	<div class="row">
+		<div class="col-lg-9 col-md-8 col-sm-8">
+			<img class="img-responsive img-rounded vijest-banner" src="{{ asset('img/news/' . $story->image) }}">
+		</div>
+		<div class="col-lg-3 col-md-4 col-sm-4">
+			<h2><small>Vijest</small><br/>{{ $story->title }}</h2>
+			<p>{{ $story->user->getFullName() }}<br>{{ $story->created_at->format('d.m.Y. H:i') }}</p>
+			@if(Auth::check())
+				@if(Auth::user()->isStoryAuthor($story))
+					<p>
+					<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Uredi vijest</a>
+					<span class='glyphicon glyphicon-minus'></span>
+					<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Obriši vijest</a>
+					</p>
+				@endif
+			@endif
+		</div>
+	</div>
 </div>
 <div class="vijest-body">
 	<div class="container">
@@ -14,18 +30,6 @@
 			{!! Markdown::setMarkupEscaped(true)->parse($story->body) !!}
 		</div>
 	</div>
-</div>
-<div class="container">
-	<p style='text-align:center'>{{ $story->user->getFullName() }}, {{ $story->created_at->diffForHumans() }}</p>
-	@if(Auth::check())
-		@if(Auth::user()->isStoryAuthor($story))
-			<p style='text-align:center'>
-			<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Uredi vijest</a>
-			<span class='glyphicon glyphicon-minus'></span>
-			<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Obriši vijest</a>
-			</p>
-		@endif
-	@endif
 </div>
 </div>
 @stop
