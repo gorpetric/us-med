947f15b80373f6b6e671d6ae39ddc503a95bf19f
Goran <gorpetric@foi.hr>
2015-11-20 12:11:55 +0100

    More news design

--- resources/views/news/story.blade.php
+++ resources/views/news/story.blade.php
@@ -4,24 +4,22 @@
 
 @section('content')
 <div class="vijest">
-<div class="container">
-	<div class="row">
-		<div class="col-lg-9 col-md-8 col-sm-8">
-			<img class="img-responsive img-rounded vijest-banner" src="{{ asset('img/news/' . $story->image) }}">
-		</div>
-		<div class="col-lg-3 col-md-4 col-sm-4">
-			<h2><small>Vijest</small><br/>{{ $story->title }}</h2>
-			<p>{{ $story->user->getFullName() }}<br>{{ $story->created_at->format('d.m.Y. H:i') }}</p>
-			@if(Auth::check())
-				@if(Auth::user()->isStoryAuthor($story))
-					<p>
-					<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Uredi vijest</a>
-					<span class='glyphicon glyphicon-minus'></span>
-					<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Obriši vijest</a>
-					</p>
-				@endif
+<div class="container vj">
+	<div class="vijest-banner">
+		<img class="img-responsive img-rounded" src="{{ asset('img/news/' . $story->image) }}">
+	</div>
+	<div class="vijest-info">
+		<h2><small>Vijest</small><br/>{{ $story->title }}</h2>
+		<p>{{ $story->user->getFullName() }}<br>{{ $story->created_at->format('d.m.Y. H:i') }}</p>
+		@if(Auth::check())
+			@if(Auth::user()->isStoryAuthor($story))
+				<p>
+				<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Uredi vijest</a>
+				<span class='glyphicon glyphicon-minus'></span>
+				<a href="{{ route('news.edit', ['slug' => $story->slug]) }}">Obriši vijest</a>
+				</p>
 			@endif
-		</div>
+		@endif
 	</div>
 </div>
 <div class="vijest-body">
