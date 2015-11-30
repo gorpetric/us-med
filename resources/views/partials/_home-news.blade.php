7961cb70fc09463d06a7c5ee855a10c7b13485ef
Goran <gorpetric@foi.hr>
2015-11-30 16:19:33 +0100

    Home styling idk whatnot already

--- resources/views/partials/_home-news.blade.php
+++ resources/views/partials/_home-news.blade.php
@@ -2,11 +2,15 @@
 @foreach($news as $story)
 	<div class="col-sm-6 col-md-3">
 		<div class="story-out">
-			<img class='img-responsive img-rounded' src="{{ asset('img/news/' . $story->image) }}">
-			<p class="help-block"><span class="glyphicon glyphicon-time"></span> {{ $story->created_at->format('d.m.Y.') }}</p>
-			<h4><a href="{{ route('news.story', ['slug'=>$story->slug]) }}" title="{{ $story->title }}">{{ str_limit($story->title, 50) }}</a></h4>
-			<p class='strip-story'>{{ stripMarkdown(str_limit($story->body, 100)) }}</p>
-			<a class='btn btn-default' href="{{ route('news.story', ['slug'=>$story->slug]) }}">Pročitaj vijest</a>
+			<img class='img-responsive' src="{{ asset('img/news/' . $story->image) }}">
+			<div class="ostatak">
+				<div class="info">
+					<p class="help-block"><span class="glyphicon glyphicon-time"></span> {{ $story->created_at->format('d.m.Y.') }}</p>
+					<h4><a href="{{ route('news.story', ['slug'=>$story->slug]) }}" title="{{ $story->title }}">{{ str_limit($story->title, 50) }}</a></h4>
+					<p class='strip-story'>{{ stripMarkdown(str_limit($story->body, 100)) }}</p>
+				</div>
+				<a class='btn btn-default' href="{{ route('news.story', ['slug'=>$story->slug]) }}">Pročitaj vijest</a>
+			</div>
 		</div>
 	</div>
 @endforeach
