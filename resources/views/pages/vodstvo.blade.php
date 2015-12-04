9b741b1436ae740204c438715d771f166715c84c
Goran <gorpetric@foi.hr>
2015-12-04 16:50:15 +0100

    Leadership page dummy info

--- resources/views/pages/vodstvo.blade.php
+++ resources/views/pages/vodstvo.blade.php
@@ -8,39 +8,127 @@
 <p class="help-block">Coming soon...</p>
 <div class="row">
 	<div class="col-sm-4 lead-out">
-		<img class='img-responsive' src="https://placehold.it/500x500">
-		<h4>
-			<span class="glyphicon glyphicon-star"></span>
-			Pred Sjednik
-		</h4>
-		<div class="sm">
-			<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
-			<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
-			<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>
+				<span class="glyphicon glyphicon-star"></span>
+				Pred Sjednik
+			</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
 		</div>
 	</div>
 	<div class="col-sm-4 lead-out">
-		<img class='img-responsive' src="https://placehold.it/500x500">
-		<h4>
-			<span class="glyphicon glyphicon-star-empty"></span>
-			Zam Jenik
-		</h4>
-		<div class="sm">
-			<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
-			<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
-			<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>
+				<span class="glyphicon glyphicon-star-empty"></span>
+				Zam Jenik
+			</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
 		</div>
 	</div>
 	<div class="col-sm-4 lead-out">
-		<img class='img-responsive' src="https://placehold.it/500x500">
-		<h4>
-			<span class="glyphicon glyphicon-euro"></span>
-			Taj Nik
-		</h4>
-		<div class="sm">
-			<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
-			<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
-			<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>
+				<span class="glyphicon glyphicon-euro"></span>
+				Taj Nik
+			</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
+		</div>
+	</div>
+</div>
+<hr style="border-color:#262626">
+<div class="row">
+	<div class="col-sm-3 lead-out">
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>O Stali</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3 lead-out">
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>Os Tali</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3 lead-out">
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>Ost Ali</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3 lead-out">
+		<div class="lead-in">
+			<div class="info">
+				<img class='img-responsive img-rounded' src="https://placehold.it/500x250">
+				<div class="text">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non reiciendis architecto consequuntur.</p>
+				</div>
+			</div>
+			<h4>Osta Li</h4>
+			<div class="sm">
+				<a href="#"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+				<a href="#"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+			</div>
 		</div>
 	</div>
 </div>
