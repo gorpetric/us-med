bed4fc5b35d71722760074e06154682cf2a70760
Goran <gorpetric@foi.hr>
2015-12-11 19:43:50 +0100

    Leadership page with data from db

--- app/Lead.php
+++ app/Lead.php
@@ -27,4 +27,30 @@ class Lead extends Model
         return $this->first_name . ' ' . $this->last_name;
     }
 
+    public function img()
+    {
+        return "<img src='".asset('img/udruga/vodstvo/'.$this->image.'')."' class='img-responsive img-rounded'>";
+    }
+
+    public function mainRow()
+    {
+        if($this->president || $this->substitute || $this->secretary){
+            return true;
+        }
+        return false;
+    }
+
+    public function mainGlyphicon()
+    {
+        if($this->president){
+            return "<span class='glyphicon glyphicon-star'></span>";
+        } 
+        if($this->substitute){
+            return "<span class='glyphicon glyphicon-star-empty'></span>";
+        } 
+        if($this->secretary){
+            return "<span class='glyphicon glyphicon-euro'></span>";
+        }
+        return null;
+    }
 }
--- resources/views/lead/index.blade.php
+++ resources/views/lead/index.blade.php
@@ -8,13 +8,77 @@
 		<div class="flex-container">
 			<div class='title'>
 				<h1><a href="{{ route('lead.index') }}"><span class="glyphicon glyphicon-king"></span> Vodstvo</a></h1>
+				<p class='help-block'>
+					<span class="glyphicon glyphicon-star"></span> Predsjednik
+					<span class="glyphicon glyphicon-star-empty"></span> Zamjenik
+					<span class="glyphicon glyphicon-euro"></span> Tajnik
+				</p>
 			</div>
 		</div>
 	</div>
 </header>
 <div class="container">
-@foreach($leads as $lead)
-<p>{{ $lead->getFullName() }}</p>
-@endforeach
+	<div class="row">
+	@foreach($leads as $lead)
+		@if($lead->mainRow())
+			<div class="col-sm-4 lead-out">
+				<div class="lead-in">
+					<div class="info">
+						{!! $lead->img() !!}
+						<div class="text">{{ $lead->description }}</div>
+					</div>
+					<h4>
+						{!! $lead->mainGlyphicon() !!}
+						{{ $lead->getFullName() }}
+					</h4>
+					<div class="sm">
+						@if($lead->facebook)
+							<a target='_blank' href="{{ $lead->facebook }}"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+						@endif
+						@if($lead->youtube)
+							<a target='_blank' href="{{ $lead->youtube }}"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+						@endif
+						@if($lead->instagram)
+							<a target='_blank' href="{{ $lead->instagram }}"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+						@endif
+						@if($lead->twitter)
+							<a target='_blank' href="{{ $lead->twitter }}"><img src="{{ asset('img/social-media-icons/twitter.png') }}"></a>
+						@endif
+						@if($lead->linkedin)
+							<a target='_blank' href="{{ $lead->linkedin }}"><img src="{{ asset('img/social-media-icons/linkedin.png') }}"></a>
+						@endif
+					</div>
+				</div>
+			</div>
+		@else
+			<div class="col-sm-3 lead-out">
+				<div class="lead-in">
+					<div class="info">
+						{!! $lead->img() !!}
+						<div class="text">{{ $lead->description }}</div>
+					</div>
+					<h4>{{ $lead->getFullName() }}</h4>
+					<div class="sm">
+						@if($lead->facebook)
+							<a target='_blank' href="{{ $lead->facebook }}"><img src="{{ asset('img/social-media-icons/facebook.png') }}"></a>
+						@endif
+						@if($lead->youtube)
+							<a target='_blank' href="{{ $lead->youtube }}"><img src="{{ asset('img/social-media-icons/youtube.png') }}"></a>
+						@endif
+						@if($lead->instagram)
+							<a target='_blank' href="{{ $lead->instagram }}"><img src="{{ asset('img/social-media-icons/instagram.png') }}"></a>
+						@endif
+						@if($lead->twitter)
+							<a target='_blank' href="{{ $lead->twitter }}"><img src="{{ asset('img/social-media-icons/twitter.png') }}"></a>
+						@endif
+						@if($lead->linkedin)
+							<a target='_blank' href="{{ $lead->linkedin }}"><img src="{{ asset('img/social-media-icons/linkedin.png') }}"></a>
+						@endif
+					</div>
+				</div>
+			</div>
+		@endif
+	@endforeach
+	</div>
 </div>
 @stop
